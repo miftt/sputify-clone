e7999d8d2684026327e329f2d3c08919194fc5dc
miftt <miftahfauzi012@gmail.com>
2023-11-15 10:29:56 +0700

    first commit for all work

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next'
+import { Figtree } from 'next/font/google'
+import './globals.css'
+import Sidebar from '@/components/Sidebar'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Spotify Clone - Mifuzi',
+  description: 'Listening to Music!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <SupabaseProvider>
+          <UserProvider>
+            <Sidebar>
+              {children}
+            </Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
+        </body>
+    </html>
+  )
+}
